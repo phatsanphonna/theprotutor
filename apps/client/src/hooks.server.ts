d69708e92267ff2c774563a44c4902f6b08b6fae
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-09-14 15:18:15 +0700

    Feat: Init TRPC

--- apps/client/src/hooks.server.ts
+++ apps/client/src/hooks.server.ts
@@ -1,5 +1,6 @@
 import { injectSession } from '$lib/server/middlewares';
+import { trpcHandler } from '$lib/trpc/handle';
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = sequence(injectSession)
+export const handle: Handle = sequence(injectSession, trpcHandler)
