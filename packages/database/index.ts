178b321a68f7d30599ae7646b8dd9753e85f7f54
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-10-07 14:28:56 +0700

    Fix: Fucking Prisma cyka blyat!!!

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,24 +1,4 @@
-export type {
-  $Enums,
-  Account,
-  Appointment,
-  Assignment,
-  Config,
-  File,
-  FileType,
-  Lesson,
-  Log,
-  LogActivity,
-  LoginRequest,
-  Prisma,
-  PrismaClient,
-  PrismaPromise,
-  Role,
-  Score, Scoreboard,
-  Session,
-  Student,
-  Teacher,
-  User,
-  VerificationToken
-} from "@prisma/client";
-export * from './db';
+export * from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient({ log: ['query'] });
