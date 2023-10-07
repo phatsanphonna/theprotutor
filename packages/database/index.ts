6d8dd0259026a5cb5f1f3115f9b2eb1c591b6693
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-10-07 13:52:02 +0700

    Fix: Prisma Type

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,2 +1,24 @@
-export * from "@prisma/client";
-export * from './db'
+export type {
+  $Enums,
+  Account,
+  Appointment,
+  Assignment,
+  Config,
+  File,
+  FileType,
+  Lesson,
+  Log,
+  LogActivity,
+  LoginRequest,
+  Prisma,
+  PrismaClient,
+  PrismaPromise,
+  Role,
+  Score, Scoreboard,
+  Session,
+  Student,
+  Teacher,
+  User,
+  VerificationToken
+} from "@prisma/client";
+export * from './db';
