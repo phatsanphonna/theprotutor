b7637150671c0067be0c62c3de73b79b45bf4413
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-10-08 23:18:33 +0700

    Fix: index page in admin app

--- apps/admin/src/routes/+page.svelte
+++ apps/admin/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col justify-center items-center">
-	<h2>ระบบจัดการหลังบ้าน THE PRO TUTOR</h2>
+<div class="flex flex-col justify-center items-center gap-4 h-full">
+	<h2 class="font-bold text-2xl">ระบบจัดการหลังบ้าน THE PRO TUTOR</h2>
 	<button class="btn variant-filled-primary">เข้าสู่ระบบด้วยบัญชี Google</button>
 </div>
