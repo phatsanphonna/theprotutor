6a0a3547f032901f28d64a139cd0a6c24ddc9e3d
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2023-10-08 18:58:43 +0700

    Edit: app.html

--- apps/client/src/routes/+error.svelte
+++ apps/client/src/routes/+error.svelte
@@ -4,10 +4,13 @@
 </script>
 
 <div
-  class="container mx-auto px-4 md:px-0 py-8 min-h-screen flex flex-col justify-center items-center gap-4"
+  class="container mx-auto h-full px-4 md:px-0 py-8 flex flex-col justify-center items-center gap-4 font-sans"
 >
   <div class="text-center">
-    <h1 class="h1 font-bold">{$page.status}</h1>
-    <p class="text-lg">{$page.error?.message}</p>
+    <h1 class="font-bold text-8xl">{$page.status}</h1>
+    <p class="text-2xl">{$page.error?.message}</p>
   </div>
+
+  <hr class="!border-t-2 w-3/5">
+  <span>Back to <a class="anchor" href="/dashboard/assignments">dashboard</a>.</span>
 </div>
